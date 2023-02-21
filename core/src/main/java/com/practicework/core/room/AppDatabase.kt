fd8128e0d90320a47d9beb84aa9640128af7f3d2
Tigran Avdalyan <tikoavd13@gmail.com>
2023-02-22 00:40:42 +0400

    after rebasing commit

--- core/src/main/java/com/practicework/core/room/AppDatabase.kt
+++ core/src/main/java/com/practicework/core/room/AppDatabase.kt
@@ -5,11 +5,11 @@ import androidx.room.RoomDatabase
 import com.practicework.core.room.dao.AllUsersDao
 import com.practicework.core.room.dao.ReposDao
 import com.practicework.core.room.dao.UserDao
+import com.practicework.core.room.models.AllUserDbModel
 import com.practicework.core.room.models.RepoEntity
 import com.practicework.core.room.models.UserEntity
 
-@Database(entities = [UserEntity::class, RepoEntity::class], version = 1)
-@Database(entities = [UserDbModel::class, RepoDbModel::class, AllUserDbModel::class], version = 1)
+@Database(entities = [UserEntity::class, RepoEntity::class, AllUserDbModel::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun userDao() : UserDao
